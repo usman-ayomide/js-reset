4cd92ede6abf204ec27ada447ba22100f4b92a84
Big Boss <oyebamijiusmanayomide@gmail.com>
2026-05-31 21:24:37 +0100

    Passing Data with Ejs

--- ejs/4.2 Passing Data/index.js
+++ ejs/4.2 Passing Data/index.js
@@ -3,12 +3,25 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+var filled;
+var results;
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  filled = false;
+  res.render("index.ejs", {filled: filled});
+});
 
-app.post("/submit", (req, res) => {});
+app.post("/submit", (req, res) => {
+  const fName = req.body.fName.length;
+  const lName = req.body.lName.length;
+  results = fName + lName;
+  filled = true;
+
+  res.render("index.ejs", {results: results, filled: filled});
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
