1de89a22f77a13274932e994a8a9b3a79153995b
Big Boss <oyebamijiusmanayomide@gmail.com>
2026-06-04 22:09:26 +0100

    Fetching data with json

--- daily-practice/day35/day35.js
+++ daily-practice/day35/day35.js
@@ -1,9 +1,9 @@
 import fs from "fs/promises";
 
-async function updateScores(){
+async function updateScore() {
     try{
-        const fileData = await fs.readFile("data.json", "utf8");
-        const students = JSON.parse(fileData)
+        const fileContent = await fs.readFile("data.json", "utf8");
+        const students = JSON.parse(fileContent);
 
         students.users.forEach(student => {
             student.score += 10;
@@ -11,13 +11,14 @@ async function updateScores(){
             if(student.score >= 90) student.grade = "A";
             else if(student.score >= 75) student.grade = "B";
             else student.grade = "C";
-            console.log("Update")
+
+            console.log("updated");
         });
 
         await fs.writeFile("data.json", JSON.stringify(students, null, 2), "utf8");
-
-    } catch (err){
+    }
+    catch(err){
         console.log(err);
     }
 }
-updateScores();
+updateScore();
