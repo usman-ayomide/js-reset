ee1d5fe6e585f592c9a0a2b123d9e52ed0f5e7e0
Big Boss <oyebamijiusmanayomide@gmail.com>
2026-06-05 23:56:09 +0100

    Number game with node.js

---
+++ daily-practice/day37/index.js
@@ -0,0 +1,51 @@
+import readline from "readline";
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function ask(question) {
+    return new Promise((resolve) => {
+        rl.question(question, (answer) => {
+            resolve(answer);
+        });
+    });
+}
+
+
+let random = Math.floor(Math.random() * 100) + 1;
+let attempts = 0;
+
+async function numGame(){
+    const guess = await ask("Guess: ");
+    attempts++;
+
+    const num = parseInt(guess);
+
+    if(num < random) {
+        console.log("Too low");
+        await numGame(); 
+    } else if(num > random) {
+        console.log("Too high");
+        await numGame(); 
+    } else {
+        console.log(`Correct in ${attempts} attempts`);
+        return;
+    }
+}
+
+
+async function start() {
+    random = Math.floor(Math.random() * 100) + 1;
+    attempts = 0;
+    await numGame();
+    
+    const playAgain = await ask("Play again? (yes/no): ");
+    if(playAgain === "yes") {
+        start(); 
+    } else {
+        rl.close();
+    }
+}
+start();
