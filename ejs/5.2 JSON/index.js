cc60518bd96665f5206611f1947695233fa90252
Big Boss <oyebamijiusmanayomide@gmail.com>
2026-06-09 00:58:07 +0100

    Ejs Challenge

--- ejs/5.2 JSON/index.js
+++ ejs/5.2 JSON/index.js
@@ -4,25 +4,29 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
-//read, parse, modify, stringify
+//read, parse, modify, stringify, write
 const recipeJSON =
   '[{"id": "0001","type": "taco","name": "Chicken Taco","price": 2.99,"ingredients": {"protein": {"name": "Chicken","preparation": "Grilled"},  "salsa": {"name": "Tomato Salsa","spiciness": "Medium"},  "toppings": [{"name": "Lettuce",  "quantity": "1 cup",  "ingredients": ["Iceberg Lettuce"]  },      {"name": "Cheese",  "quantity": "1/2 cup",  "ingredients": ["Cheddar Cheese", "Monterey Jack Cheese"]  },      {"name": "Guacamole",  "quantity": "2 tablespoons",  "ingredients": ["Avocado", "Lime Juice", "Salt", "Onion", "Cilantro"]  },      {"name": "Sour Cream",  "quantity": "2 tablespoons",  "ingredients": ["Sour Cream"]  }      ]    }  },{"id": "0002","type": "taco","name": "Beef Taco","price": 3.49,"ingredients": {"protein": {"name": "Beef","preparation": "Seasoned and Grilled"},  "salsa": {"name": "Salsa Verde","spiciness": "Hot"},  "toppings": [{"name": "Onions",  "quantity": "1/4 cup",  "ingredients": ["White Onion", "Red Onion"]  },      {"name": "Cilantro",  "quantity": "2 tablespoons",  "ingredients": ["Fresh Cilantro"]  },      {"name": "Queso Fresco",  "quantity": "1/4 cup",  "ingredients": ["Queso Fresco"]  }      ]    }  },{"id": "0003","type": "taco","name": "Fish Taco","price": 4.99,"ingredients": {"protein": {"name": "Fish","preparation": "Battered and Fried"},  "salsa": {"name": "Chipotle Mayo","spiciness": "Mild"},  "toppings": [{"name": "Cabbage Slaw",  "quantity": "1 cup",  "ingredients": [    "Shredded Cabbage",    "Carrot",    "Mayonnaise",    "Lime Juice",    "Salt"          ]  },      {"name": "Pico de Gallo",  "quantity": "1/2 cup",  "ingredients": ["Tomato", "Onion", "Cilantro", "Lime Juice", "Salt"]  },      {"name": "Lime Crema",  "quantity": "2 tablespoons",  "ingredients": ["Sour Cream", "Lime Juice", "Salt"]  }      ]    }  }]';
+const recipes = JSON.parse(recipeJSON);
+let clicked;
+// let recipe;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index.ejs", {clicked: false});
 });
 
 
 app.post("/recipe", (req, res) => {
-  const recipes = JSON.parse(recipeJSON);
+    
   const clicked = req.body.choice;
-  
 
-  res.render("index.ejs", {clicked: clicked});
+  const selectedRecipe = recipes.find(re => re.ingredients.protein.name.toLowerCase() === clicked);
+  
+  res.render("index.ejs", {recipes: selectedRecipe, clicked: true});
   
 });
 
