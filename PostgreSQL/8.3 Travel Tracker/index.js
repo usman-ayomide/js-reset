215d693664716533078e529c458675baa62e93c2
Big Boss <oyebamijiusmanayomide@gmail.com>
2026-06-17 23:45:23 +0100

    This is full of errors

--- PostgreSQL/8.3 Travel Tracker/index.js
+++ PostgreSQL/8.3 Travel Tracker/index.js
@@ -18,28 +18,30 @@ db.connect();
 let countries = [];
 let total = 0;
 
-db.query("SELECT * FROM visited_countries", (err, res) => {
-  if(err){
-    console.error("Error executing query", err.stack);
-  }else{
-    countries = res.rows;
-    console.log("You've been to:", countries.length);
-    console.log(countries);
-  }
-  //db.end();
-});
+// db.query("SELECT * FROM visited_countries", (err, res) => {
+//   if(err){
+//     console.error("Error executing query", err.stack);
+//   }
+//   else{
+//     countries = res.rows;
+//     console.log("You've been to:", countries.length);
+//     console.log(countries);
+//   }
+//   //db.end();
+// });
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
-  //Write your code here.
+
   try{
     total = countries.length;
-    const code = countries.map(country => country.country_code);
+    const result = await db.query("SELECT country_code FROM visited_countries");
+    const codes = result.rows.map(row => row.country_code);
     
     res.render("index.ejs", {
-      countries: code, total: code.length
+      countries: codes, total: codes.length
     });
   }
   catch(error){
@@ -49,6 +51,49 @@ app.get("/", async (req, res) => {
       error: error.message, countries: [], total: 0
     });
   }
+});
+
+
+app.post("/add", async (req, res) => { 
+  const input =  req.body.country;
+  console.log(input);
+
+  try{
+    const result = await db.query(
+      "SELECT country_code FROM countries WHERE country_name = $1;",
+      [input]
+    );
+
+    if(result.rows.length == 0){
+      res.status(404).send("Country does not exist");
+      return;
+    } 
+
+    const code = result.rows[0].country_code;
+
+    await db.query(
+      "INSERT INTO visited_countries (country_code) VALUES ($1);",
+      [code]
+    )
+
+    const visited = await db.query(
+      "SELECT countries.country_code FROM visited_countries JOIN countries ON visited_countries.country_code = countries.country_code;"
+    );
+    
+    countries = visited.rows.map(country => country.country_code);
+    
+    console.log("You've been to:", countries.length);
+
+    res.redirect("/");
+  }
+  catch(error){
+    console.error("Error getting countries", error.stack);
+    
+    res.render("index.ejs", {
+      error: error.message, countries: [], total: 0
+    });
+  }
+  //db.end();
 });
 
 app.listen(port, () => {
