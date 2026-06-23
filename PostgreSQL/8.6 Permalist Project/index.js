48071033917a454d000f5e69e3fdcaed77402bfc
Big Boss <oyebamijiusmanayomide@gmail.com>
2026-06-23 23:59:14 +0100

    remove exposed credentials, use env vars

--- PostgreSQL/8.6 Permalist Project/index.js
+++ PostgreSQL/8.6 Permalist Project/index.js
@@ -1,16 +1,18 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 const port = 3000;
 
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "permalist",
-  password: "123-ayomide",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 db.connect();
 
