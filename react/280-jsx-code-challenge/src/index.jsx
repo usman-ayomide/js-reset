8327f66dc71ca90f8fbc452a226c206fc5973132
Big Boss <oyebamijiusmanayomide@gmail.com>
2026-07-03 23:56:37 +0100

    jsx code challenge

---
+++ react/280-jsx-code-challenge/src/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <div>
+        <h1>Introduction!</h1>
+
+        <ul>
+            <li>i</li>
+            <li>me</li>
+            <li>myself</li>
+        </ul>
+    </div>
+);
