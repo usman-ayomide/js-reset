09c6b6d8ec8f5b5b577092dd350bbcbd03a02c20
Big Boss <oyebamijiusmanayomide@gmail.com>
2026-07-03 23:37:12 +0100

    introduction to jsx

---
+++ react/279-intro-to-jsx/src/index.jsx
@@ -0,0 +1,16 @@
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <div>
+        <h1>Hello World!</h1>
+        <p>This is a paragraph</p>
+    </div>
+);
