154da6090b83f3061a83907ebf2d9d1bf16268b7
Big Boss <oyebamijiusmanayomide@gmail.com>
2026-07-06 00:27:35 +0100

    add beyonce and jack

---
+++ react/295-react-props/src/index.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function Card(props){
+
+  console.log(props)
+  return(
+    <div>
+      <h2>{props.name}</h2>
+      <img
+        src={props.img}
+        alt="avatar_img"
+      />
+      <p>{props.tel}</p>
+      <p>{props.email}</p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <div>
+    <h1>My Contacts</h1>
+
+    <Card name="Beyonce" img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg" 
+    tel="+123 456 789" email="b@beyonce.com"/>
+
+    <Card name="jack Bauer" img="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
+    tel="" email=""/>
+
+    <Card name="Jack Bauer" img="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
+    tel="+987 654 321" email="jack@nowhere.com"/>
+
+    {/* <h2>Chuck Norris</h2>
+    <img
+      src="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
+      alt="avatar_img"
+    />
+    <p>+918 372 574</p>
+    <p>gmail@chucknorris.com</p> */}
+  </div>
+);
