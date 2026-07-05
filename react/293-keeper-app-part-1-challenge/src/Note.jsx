573a887941851c4406abec44ca18318c4dd1858d
Big Boss <oyebamijiusmanayomide@gmail.com>
2026-07-06 00:05:29 +0100

    Keeper app part 1

---
+++ react/293-keeper-app-part-1-challenge/src/Note.jsx
@@ -0,0 +1,10 @@
+function Note(){
+    return(
+        <div className="note">
+            <h1>This is a title</h1>
+            <p>This is a paragraph</p>
+        </div>
+    );
+}
+
+export default Note;
