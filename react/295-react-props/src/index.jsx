451d95747adebf480b5b47bc8613c48b1388f92a
Big Boss <oyebamijiusmanayomide@gmail.com>
2026-07-06 22:28:01 +0100

    react props

--- react/295-react-props/src/index.jsx
+++ react/295-react-props/src/index.jsx
@@ -1,15 +1,12 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 
 function Card(props){
-
-  console.log(props)
   return(
-    <div>
+    <div className="my-style">
       <h2>{props.name}</h2>
       <img
         src={props.img}
-        alt="avatar_img"
+        alt={props.alt}
       />
       <p>{props.tel}</p>
       <p>{props.email}</p>
@@ -23,20 +20,12 @@ root.render(
     <h1>My Contacts</h1>
 
     <Card name="Beyonce" img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg" 
-    tel="+123 456 789" email="b@beyonce.com"/>
-
-    <Card name="jack Bauer" img="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
-    tel="" email=""/>
+    alt="Beyonce_image" tel="+123 456 789" email="b@beyonce.com"/>
 
     <Card name="Jack Bauer" img="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
-    tel="+987 654 321" email="jack@nowhere.com"/>
+    alt="Jack_Bauer_image" tel="+345 783 974" email=""/>
 
-    {/* <h2>Chuck Norris</h2>
-    <img
-      src="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
-      alt="avatar_img"
-    />
-    <p>+918 372 574</p>
-    <p>gmail@chucknorris.com</p> */}
+    <Card name="Chuck Norris" img="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
+    alt="Chuck_Norris_image" tel="+918 372 574" email="gmail@chucknorris.com"/>
   </div>
 );
