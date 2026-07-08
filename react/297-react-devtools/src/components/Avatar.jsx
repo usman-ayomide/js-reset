d81aa4c5add584b9e7f86284c520de57a87d4c43
Big Boss <oyebamijiusmanayomide@gmail.com>
2026-07-08 23:01:09 +0100

    react dev tools

---
+++ react/297-react-devtools/src/components/Avatar.jsx
@@ -0,0 +1,7 @@
+function Avatar(props){
+    return (
+        <img className="circle-img" src={props.img} alt="avatar_img" />   
+    );
+}
+
+export default Avatar;
---
+++ react/297-react-devtools/src/components/Detail.jsx
@@ -0,0 +1,10 @@
+function Detail(props){
+    return(
+        <div className="bottom">
+            <p className="info">{props.tel}</p>
+            <p className="info">{props.email}</p>
+        </div>  
+    );
+}
+
+export default Detail;
