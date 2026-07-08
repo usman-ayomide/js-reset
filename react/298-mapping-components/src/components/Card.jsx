970891476b8ec51397a11e75a182bd0d4fa0fbbb
Big Boss <oyebamijiusmanayomide@gmail.com>
2026-07-08 23:32:07 +0100

    mapping component

---
+++ react/298-mapping-components/src/components/Card.jsx
@@ -0,0 +1,20 @@
+import Avatar from "./Avatar";
+import Detail from "./Detail";
+
+function Card(props) {
+  return (
+    <div className="card">
+      <div className="top">
+        <Detail className="info" detailInfo={props.id} />
+        <h2 className="name">{props.name}</h2>
+        <Avatar img={props.img} />
+      </div>
+      <div className="bottom">
+        <Detail detailInfo={props.tel} />
+        <Detail detailInfo={props.email} />
+      </div>
+    </div>
+  );
+}
+
+export default Card;
