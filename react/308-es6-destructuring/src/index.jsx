cf908ebdccca00f1340fb3852ac7e429acfc200f
Big Boss <oyebamijiusmanayomide@gmail.com>
2026-07-14 22:59:22 +0100

    remove redundancy

--- react/308-es6-destructuring/src/index.jsx
+++ react/308-es6-destructuring/src/index.jsx
@@ -10,42 +10,82 @@ import cars from "./practice";
 //console.log(food);
 
 
-const [honda, tesla] = cars;
 
-const {
-    speedStats: {topSpeed: hondaTopSpeed},
-    coloursByPopularity: [hondaTopColour]
-} = honda;
 
-const {
-    speedStats: {topSpeed: teslaTopSpeed},
-    coloursByPopularity: [teslaTopColour]
-} = tesla;
+function getStats(car){
+    const {
+        model,
+        speedStats: {topSpeed},
+        coloursByPopularity: [topColour]
+    } = car;
+    return {model, topSpeed, topColour};
+}
+
+const honda = getStats(cars[0]);
+const tesla = getStats(cars[1]);
+
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <table>
-    <thead>
-        <tr>
-        <th>Brand</th>
-        <th>Top Speed</th>
-        <th>Top Color</th>
-        </tr>        
-    </thead>
-
-    <tbody>
-        <tr>
-        <td>{tesla.model}</td>
-        <td>{teslaTopSpeed}</td>
-        <td>{teslaTopColour}</td>
-        </tr>
-        <tr>
-        <td>{honda.model}</td>
-        <td>{hondaTopSpeed}</td>
-        <td>{hondaTopColour}</td>
-        </tr>
-    </tbody>
+    <table>
+        <thead>
+            <tr>
+                <th>Brand</th>
+                <th>Top Speed</th>
+                <th>Top Color</th>
+            </tr>        
+        </thead>
         
-  </table>
+        <tbody>
+            <tr>
+                <td>{tesla.model}</td>
+                <td>{tesla.topSpeed}</td>
+                <td>{tesla.topColour}</td>
+            </tr>
+            <tr>
+                <td>{honda.model}</td>
+                <td>{honda.topSpeed}</td>
+                <td>{honda.topColour}</td>
+            </tr>
+        </tbody>
+    </table>
 );
+
+//const [honda, tesla] = cars;
+
+// const {
+//     speedStats: {topSpeed: hondaTopSpeed},
+//     coloursByPopularity: [hondaTopColour]
+// } = honda;
+
+// const {
+//     speedStats: {topSpeed: teslaTopSpeed},
+//     coloursByPopularity: [teslaTopColour]
+// } = tesla;
+
+// root.render(
+//   <table>
+//     <thead>
+//         <tr>
+//         <th>Brand</th>
+//         <th>Top Speed</th>
+//         <th>Top Color</th>
+//         </tr>        
+//     </thead>
+
+//     <tbody>
+//         <tr>
+//         <td>{tesla.model}</td>
+//         <td>{teslaTopSpeed}</td>
+//         <td>{teslaTopColour}</td>
+//         </tr>
+//         <tr>
+//         <td>{honda.model}</td>
+//         <td>{hondaTopSpeed}</td>
+//         <td>{hondaTopColour}</td>
+//         </tr>
+//     </tbody>
+        
+//   </table>
+// );
