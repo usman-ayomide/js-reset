19a956ac16e88a5478afa52ead9b3b9cfaf3db11
Big Boss <oyebamijiusmanayomide@gmail.com>
2026-07-17 00:51:24 +0100

    change spread order

--- react/313-changing-complex-state/src/components/App.jsx
+++ react/313-changing-complex-state/src/components/App.jsx
@@ -4,30 +4,39 @@ function App() {
   const [inputName, setInputName] = useState({
     fName: "", lName: ""
   });
+  //const [submittedName, setSubmittedName] = useState("");
+  //console.log(inputName);
 
   function handleChange(e){
     const {name, value} = e.target;
+    //console.log(name, value)
 
-    setInputName(previous => ({
-      ...previous, [name] : value
-    }));
-
-    //e.preventDefault();
+    setInputName((previous) => ({
+      ...previous, [name]: value
+  }));
   }
+
+  function handleSubmit(e){
+    e.preventDefault();
+    //setSubmittedName(inputName);
+    //console.log(submittedName.fName)
+  }
+
+  
 
 
   return (
     <div className="container">
-      <h1>Hello {inputName}</h1>
+      <h1>Hello {inputName.fName} {inputName.lName}</h1>
 
-      <form onSubmit={handleChange}>
+      <form onSubmit={handleSubmit}>
         <input name="fName" onChange={handleChange}
           placeholder="First Name" value={inputName.fName} 
         />
         <input name="lName" onChange={handleChange}
           placeholder="Last Name" value={inputName.lName} 
         />
-        <button type="submit" onClick={handleChange}>Submit</button>
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
