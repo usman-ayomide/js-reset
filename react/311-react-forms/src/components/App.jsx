10c9b24074bb9a732b406775e1349924dc366932
Big Boss <oyebamijiusmanayomide@gmail.com>
2026-07-16 22:58:45 +0100

    wrap button in form and change state variable

--- react/311-react-forms/src/components/App.jsx
+++ react/311-react-forms/src/components/App.jsx
@@ -3,26 +3,32 @@ import { useState } from "react";
 function App() {
 
   const [name, setName] = useState("");
-  const [click, setClick] = useState(false);
-
-  function updateName(){
-    setClick(true);
-  }
+  const [submittedName, setSubmittedName] = useState("");
 
   function handleChange(e){
     setName(e.target.value);
+
+    e.preventDefault();
+  }
+
+  function updateName(){
+    setSubmittedName(name);
   }
 
 
   return (
     <div className="container">
-      <h1>Hello {!click ? "" : name} </h1>
-      <input 
-        onChange={handleChange}
-        type="text" placeholder="What's your name?" 
-        value={name}
-      />
-      <button onClick={updateName}>Submit</button>
+      <h1>Hello {submittedName}</h1>
+
+      <form onSubmit={handleChange}>
+        <input 
+          onChange={handleChange}
+          type="text" placeholder="What's your name?" 
+          value={name}
+        />
+        <button onClick={updateName}>Submit</button>
+      </form>
+      
     </div>
   );
 }
