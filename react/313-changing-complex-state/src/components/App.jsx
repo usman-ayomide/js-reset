b55306e47439bfd4cda2a31bab5ac9bb11641d03
Big Boss <oyebamijiusmanayomide@gmail.com>
2026-07-17 00:09:06 +0100

    use spread operator

--- react/313-changing-complex-state/src/components/App.jsx
+++ react/313-changing-complex-state/src/components/App.jsx
@@ -1,26 +1,33 @@
 import { useState } from "react";
 
 function App() {
-  const [fName, setFname] = useState("");
-  const [lName, setLname] = useState("");
+  const [inputName, setInputName] = useState({
+    fName: "", lName: ""
+  });
 
-  function updateFname(e){
-    setFname(e.target.value);
+  function handleChange(e){
+    const {name, value} = e.target;
+
+    setInputName(previous => ({
+      ...previous, [name] : value
+    }));
+
+    //e.preventDefault();
   }
 
-  function handleSubmit(e){
-    e.preventDefault();
-
-  }
 
   return (
     <div className="container">
-      <h1>Hello {fName}</h1>
-      <form onSubmit={handleSubmit}>
-        <input name="fName" onChange={updateFname}
-        placeholder="First Name" value={fName} />
-        <input name="lName" placeholder="Last Name" />
-        <button type="submit" onClick={updateFname}>Submit</button>
+      <h1>Hello {inputName}</h1>
+
+      <form onSubmit={handleChange}>
+        <input name="fName" onChange={handleChange}
+          placeholder="First Name" value={inputName.fName} 
+        />
+        <input name="lName" onChange={handleChange}
+          placeholder="Last Name" value={inputName.lName} 
+        />
+        <button type="submit" onClick={handleChange}>Submit</button>
       </form>
     </div>
   );
