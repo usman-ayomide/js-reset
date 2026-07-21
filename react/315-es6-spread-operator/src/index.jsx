a67238af2ae15031177b6bd8587b41a52a5bae8f
Big Boss <oyebamijiusmanayomide@gmail.com>
2026-07-21 22:52:59 +0100

    es6 spread operator

---
+++ react/315-es6-spread-operator/src/index.jsx
@@ -0,0 +1,25 @@
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <App />
+);
+
+// const citrus = ["Lime", "Lemon", "Orange"];
+// const fruits = ["Apple", "Banana", "Coconut", ...citrus];
+
+// console.log(fruits);
+
+// const fName = {
+//     f: "James",
+//     l: "Bond"
+// };
+
+// const user = {
+//     ...fName,
+//     id: 1,
+//     username: "jamond"
+// };
+
+// console.log(user);
