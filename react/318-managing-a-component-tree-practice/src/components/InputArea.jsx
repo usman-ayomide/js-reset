a1d4205cc1d6df1b3a5ded544f857ff47b8c1526
Big Boss <oyebamijiusmanayomide@gmail.com>
2026-07-24 00:43:15 +0100

    initial commit

---
+++ react/318-managing-a-component-tree-practice/src/components/InputArea.jsx
@@ -0,0 +1,12 @@
+function InputArea() {
+  return (
+    <div className="form">
+      <input onChange={handleChange} type="text" value={inputText} />
+      <button onClick={addItem}>
+        <span>Add</span>
+      </button>
+    </div>
+  );
+}
+
+export default InputArea;
